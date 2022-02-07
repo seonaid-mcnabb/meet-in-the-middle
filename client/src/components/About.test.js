4c29c9bb42b1336a4dcf7df33964d070239e83ab
Sofia Moura <sofiamourap@gmail.com>
2022-02-07 20:41:42 +0000

    debugging the packages

--- client/src/components/About.test.js
+++ client/src/components/About.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import About from "./About";
 //test commit
 configure({ adapter: new Adapter() });
